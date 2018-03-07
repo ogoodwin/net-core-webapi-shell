abc18f1443c8b93560a2f8cca49dbf0642a3d80c
Goodwin, Otis <otisgoodwin@quickenloans.com>
2018-03-07 13:05:53 -0500

    Adding api and test project

---
+++ test/NetCoreWebApiShell.Api.Tests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using NetCoreWebApiShell.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NetCoreWebApiShell.Api.Tests.Controllers
+{
+    public class ValuesControllerTests
+    {
+        private readonly ValuesController _target;
+
+        public ValuesControllerTests()
+        {
+            _target = new ValuesController();
+        }
+
+        [Fact]
+        public void Get_ValidId_ReturnedExpectedResult()
+        {
+            //Arrange
+            var id = 1;
+            var expected = "value";
+
+            //Act
+            var result = _target.Get(id);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
